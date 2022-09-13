fb1d415771828f3a76ed764479c8f0d3ea19ee8a
Dmitriy Bubyr <d.bubyr@lancktele.com>
2022-09-13 11:30:33 +0300

    First commit

--- src/main/java/org/example/MyThreadRecursiveTask.java
+++ src/main/java/org/example/MyThreadRecursiveTask.java
@@ -6,7 +6,7 @@ import java.util.concurrent.RecursiveTask;
 public class MyThreadRecursiveTask extends RecursiveTask<Integer> {
 
     private int[] array;
-    private final int partsNum = 2;
+    private final int partsNum = 4;
 
     public MyThreadRecursiveTask(int[] array) {
         this.array = array;
@@ -18,11 +18,18 @@ public class MyThreadRecursiveTask extends RecursiveTask<Integer> {
             return Arrays.stream(array).sum();
         }
         MyThreadRecursiveTask firstHalfArrayValueSumCounter =
-            new MyThreadRecursiveTask(Arrays.copyOfRange(array, 0, array.length / 2));
+            new MyThreadRecursiveTask(Arrays.copyOfRange(array, 0, array.length / 4));
         MyThreadRecursiveTask secondHalfArrayValueSumCounter =
-            new MyThreadRecursiveTask(Arrays.copyOfRange(array, array.length / 2, array.length));
+            new MyThreadRecursiveTask(Arrays.copyOfRange(array, array.length / 4, 2 * array.length / 4));
+        MyThreadRecursiveTask thirdHalfArrayValueSumCounter =
+            new MyThreadRecursiveTask(Arrays.copyOfRange(array, 2 * array.length / 4, 3 * array.length / 4));
+        MyThreadRecursiveTask forthHalfArrayValueSumCounter =
+            new MyThreadRecursiveTask(Arrays.copyOfRange(array, 3 * array.length / 4, array.length));
         firstHalfArrayValueSumCounter.fork();
         secondHalfArrayValueSumCounter.fork();
-        return firstHalfArrayValueSumCounter.join() + secondHalfArrayValueSumCounter.join();
+        thirdHalfArrayValueSumCounter.fork();
+        forthHalfArrayValueSumCounter.fork();
+        return firstHalfArrayValueSumCounter.join() + secondHalfArrayValueSumCounter.join() +
+            thirdHalfArrayValueSumCounter.join() + forthHalfArrayValueSumCounter.join();
     }
 }
